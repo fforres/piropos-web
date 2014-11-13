f323719c38cef82d1c5bca364b50c77da792e931
forres-koding <felipe.torressepulveda@gmail.com>
2014-11-13 02:56:03 +0000

    add imagenes y metadata para fb

--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,54 @@
 var express = require('express');
 var router = express.Router();
 var apiURL = "http://fforres.koding.io:3007/api"
-  /* GET home page. */
+	/* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', {
-    title: 'Sr.piropos! -> Los mejores 10 piropos'
-  });
+	res.render('index', {
+		title: 'Sr.piropos! -> Los mejores 10 piropos'
+	});
 });
 router.get('/piropo', function(req, res) {
-  res.redirect('/');
+	res.redirect('/');
 });
 
 router.get('/piropo/:id', function(req, res) {
-  res.render('index', {
-    title: 'Sr.Piropos! - Piropo -> ' + req.params.id
-  });
+	var request = require('request')
+	request({
+		method: 'GET',
+		uri: apiURL+'/piropos/'+req.params.id,
+	}, function(error, response, body) {
+		console.log(response.statusCode)
+		if (response.statusCode == 200) {
+			var thebody = JSON.parse(body);
+			res.render('index', {
+				title: 'Sr. Piropos',
+				FBmeta: {
+					"title":"¡Que buen piropo!",
+					"site_name":"Sr. Piropos",
+					"description":thebody.texto,
+					"image":"https://c2.staticflickr.com/4/3386/3520657923_138009c63f.jpg",
+					"app_id":"861140763904895",
+					"type":"article",
+					"author":thebody.usuario,
+					"publisher":"Sr.Piropos"
+				}
+			});
+		} else {
+			res.render('index', {
+				title: 'Sr.Piropos!'
+			});
+		}
+	})
+
 });
 
 router.get('/usuario/:nombre', function(req, res) {
-  res.render('index', {
-    title: 'Sr.Piropos! - Piropos de @' + req.params.nombre
-  });
+	res.render('index', {
+		title: 'Sr.Piropos! - Piropos de @' + req.params.nombre
+	});
 });
 router.get('/*', function(req, res) {
-  res.redirect('/');
+	res.redirect('/');
 });
 
 module.exports = router;
